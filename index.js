88abcd756f198c381525e9fa82be839222848cf2
Julian Bradach <75348299+Heidenan@users.noreply.github.com>
2021-11-01 22:48:04 -0300

    Delete index.js

--- index.js
+++
@@ -1,7 +0,0 @@
-const borrar = require ("./modules/borrar")
-const escribir = require ("./modules/escribir")
-const crear = require ("./modules/crear")
-const leer = require ("./modules/leer");
-const listar = require ("./modules/listar");
-let tareas = leer ("./data/tareas.json")
-listar (tareas)
